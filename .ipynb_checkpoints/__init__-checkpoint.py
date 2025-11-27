66bab4036ad0e3fa8e668b7563e04fa88bbd4ce4
Moshe Shenhav <shenham1@biu.ac.il>
2025-11-27 12:04:51 +0200

    Stop tracking IDE and cache files as per .gitignore

--- .ipynb_checkpoints/__init__-checkpoint.py
+++
@@ -1,5 +0,0 @@
-# Top-level imports for convenience
-from .objects.sample_object import SampleObject
-from .objects.project_object import ProjectObject
-
-__all__ = ["SampleObject", "ProjectObject"]
